09098e66c0e5857143953c28e492a888395c3ff0
K Creanor <kcreanor@hotmail.com>
2017-06-11 20:16:18 +0100

    Added level timer class

---
+++ Assets/Resources/Scripts/Times/LevelTime.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelTime {
+
+    public float _S_time;
+
+    public float _A_time;
+
+    public float _B_time;
+
+    public float _C_time;
+
+    public string _rank;
+}
---
+++ Assets/Resources/Scripts/Times/LevelTimeContainer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTimeContainer : MonoBehaviour {
+
+	public List <LevelTime> _levelTimes = new List <LevelTime>();
+
+    public void Awake()
+    {
+        CheckTimes();
+    }
+
+    void CheckTimes()
+    {
+        for (int i = 0; i < _playerManager._totalLevels; i++)
+        {
+            if (_playerManager._times[i] <= _levelTimes[i]._S_time)
+            {
+                _levelTimes[i]._rank = "S";
+            }
+            else if (_playerManager._times[i] <= _levelTimes[i]._A_time)
+            {
+                _levelTimes[i]._rank = "A";
+            }
+            else if (_playerManager._times[i] <= _levelTimes[i]._B_time)
+            {
+                _levelTimes[i]._rank = "B";
+            }
+            else if (_playerManager._times[i] <= _levelTimes[i]._C_time)
+            {
+                _levelTimes[i]._rank = "C";
+            }
+            else
+            {
+                _levelTimes[i]._rank = "-";
+            }
+        }
+    }
+}

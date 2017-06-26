bce8b6247a38c24c600435a52088479a912e24e2
K Creanor <kcreanor@hotmail.com>
2017-06-26 09:29:13 +0100

    Added animations for Sc02

---
+++ Assets/Resources/Scripts/Trigger/TriggerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour {
+
+    public Animator _anim;
+
+    public bool _bool;
+
+    public int _stage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") Triggered();
+    }
+
+    public void Triggered()
+    {
+        if (_bool)
+        {
+            _anim.SetBool("Triggered", true);
+        }
+        else
+        {
+            _anim.SetInteger("Stage", _stage);
+        }
+    }
+}

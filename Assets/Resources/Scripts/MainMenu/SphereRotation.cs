e6e92a3a6f673ba46ce5feb0cfb3bc8ae5f27bc0
K Creanor <kcreanor@hotmail.com>
2017-07-26 09:27:33 +0100

    Minor tweaks

---
+++ Assets/Resources/Scripts/MainMenu/SphereRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SphereRotation : MonoBehaviour {
+
+    public Transform _sphere; 
+    public float _baseSpeed = 1f;
+    public float _speed = 1f;
+    private int inputX;
+
+    // Use this for initialization
+    void Awake () {
+        _sphere = GameObject.Find("WorldContainer_parent").GetComponent<Transform>(); 
+	}
+
+    public void FixedUpdate()
+    {
+        var x = CrossPlatformInputManager.GetAxis("Horizontal");
+        var y = CrossPlatformInputManager.GetAxis("Vertical");
+        if (Mathf.Abs(x) <= 0.12f && Mathf.Abs(y) <= 0.12f) return;  
+        x *= _speed;
+        y *= _speed;
+        _sphere.Rotate(y, x, 0, Space.World);
+    }   
+}

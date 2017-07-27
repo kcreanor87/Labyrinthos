9d922545e12ffd884bdc1b68d055fe2cbeb9c373
K Creanor <kcreanor@hotmail.com>
2017-07-27 23:22:39 +0100

    Jet booster particle effect tweaks

--- Assets/Resources/Scripts/Controls/ChangeSpeed.cs
+++ Assets/Resources/Scripts/Controls/ChangeSpeed.cs
@@ -7,32 +7,57 @@ public class ChangeSpeed : MonoBehaviour {
     public PlayerControls _playerControls;
     public _manager _sceneManager;
     public ParticleSystem _boostParticle;
+    public ParticleSystem _boostGaps;
+    public ParticleSystem _regParticle;
+    public ParticleSystem _regGaps;
 
     private void Awake()
     {
         _sceneManager = GameObject.Find("UI").GetComponent<_manager>();
         _boostParticle = GameObject.Find("BoostParticles").GetComponent<ParticleSystem>();
         _boostParticle.Stop();
+        _boostGaps = GameObject.Find("BoostGaps").GetComponent<ParticleSystem>();
+        _boostGaps.Stop();
+        _regGaps = GameObject.Find("Jet_gaps").GetComponent<ParticleSystem>();
+        _regParticle = GameObject.Find("Jet").GetComponent<ParticleSystem>();
         _playerControls = GameObject.Find("Player").GetComponent<PlayerControls>();
     }
 
     public void FixedUpdate()
     {
         if (!_sceneManager._inMenu) return;
-        if (_playerControls._boost && !_boostParticle.IsAlive()) _boostParticle.Play();
-        else if (!_playerControls._boost) _boostParticle.Stop();
+        if (_playerControls._boost && !_boostParticle.IsAlive())
+        {
+            _boostGaps.Play();
+            _boostParticle.Play();
+            _regParticle.Stop();
+            _regGaps.Stop();
+        }
+        else if (!_playerControls._boost)
+        {
+            _boostParticle.Stop();
+            _boostGaps.Stop();
+            _regParticle.Play();
+            _regGaps.Play();
+        }
     }
 
     public void SpeedUp()
     {
         _playerControls._boost = true;
         _boostParticle.Play();
+        _boostGaps.Play();
+        _regParticle.Stop();
+        _regGaps.Stop();
     }
 
     public void SlowDown()
     {
         _playerControls._brake = true;
         _boostParticle.Stop();
+        _boostGaps.Stop();
+        _regParticle.Play();
+        _regGaps.Play();
     }
 
     public void ReturnToNormal()
@@ -40,5 +65,8 @@ public class ChangeSpeed : MonoBehaviour {
         _playerControls._boost = false;
         _playerControls._brake = false;
         _boostParticle.Stop();
+        _boostGaps.Stop();
+        _regParticle.Play();
+        _regGaps.Play();
     }
 }

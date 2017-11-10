cdafc8ce8d51657cc7af13280febc9f0582c65b4
K Creanor <kcreanor@hotmail.com>
2017-11-10 09:16:17 +0000

    New Wall style MaxScripts added

--- Assets/_Scene_Content/Scripts/Textures/FadingTexture.cs
+++ Assets/_Scene_Content/Scripts/Textures/FadingTexture.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class FadingTexture : MonoBehaviour {
 
-    public float _changeSpeed = 0.25f;
-    public float _maxAlpha = 150;
-    public float _minAlpha = 60;
+    public float _changeSpeed = 0.12f;
+    public float scrollSpeed = 0.1f;
+    public float _maxAlpha = 100;
+    public float _minAlpha = 30;
     private float value;
     private bool increase;
     public Renderer rend;
     public Color _colour;
-    public int _materialIndex = 2;
+    public int _materialIndex = 1;
 
     void Start()
     {
@@ -30,6 +31,8 @@ public class FadingTexture : MonoBehaviour {
         else value -= _changeSpeed;
         Color color = new Color(_colour.r, _colour.g, _colour.b, value);
         rend.materials[_materialIndex].color = color;
-        rend.materials[_materialIndex].SetFloat("_MKGlowTexStrength", value * 6.0f);
+        rend.materials[_materialIndex].SetFloat("_MKGlowTexStrength", value * 3.0f);
+        float offset = Time.time * scrollSpeed;
+        rend.materials[_materialIndex].SetTextureOffset("_MKGlowTex", new Vector2(0, offset));
     }
 }
